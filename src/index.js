e7986383131806d8d5a88e37beaffb849f86cb64
Mariam Mekhrishvili <mariamekh@github.com>
2021-06-28 23:15:23 +0400

    feat: implement root component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import {
-    async
-} from "fast-glob";
 import "regenerator-runtime/runtime";
-import {API_KEY, API_URL,URL_CHARACTERS,URL_COMICS } from "./constants/api" 
-import getDataApi from "./utils/getDataApi"
 
-(async () => {
-    const data = await getDataApi.getData(API_URL+URL_COMICS);
-    console.log(data)
+import App from "./components/App"
+
+
+(async () =>{
+    App.render();
 })();
